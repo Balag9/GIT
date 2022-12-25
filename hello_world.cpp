bee5bdf9c21a6c2f7bd2dd6f4f7939ffcb32f432
Balag9 <121099168+Balag9@users.noreply.github.com>
2022-12-25 18:31:09 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -7,7 +7,6 @@ main()
 
 {
 
-<<<<<<< HEAD
 std::string str;//variables
 
 getline(std::cin, str);//enter your name
@@ -15,11 +14,3 @@ getline(std::cin, str);//enter your name
 std::cout << "Hello World from " << str;//prints intended line
 
 }
-=======
-  std::string str; //variables
-
-  getline(std::cin, str); //enter your name
-
-  std::cout << "Hello World from " << str; //prints intended line
-}
->>>>>>> c545b64 (new Mozilla style)
